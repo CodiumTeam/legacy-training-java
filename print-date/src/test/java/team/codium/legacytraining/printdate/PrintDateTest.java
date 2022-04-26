a22e546ec97ce3d021e14b7248b2c8bd7bc7d756
Hugo Chinchilla <hugo@codium.team>
2022-04-26 15:20:46 +0200

    print date

--- print-date/src/test/java/team/codium/legacytraining/printdate/PrintDateTest.java
+++ print-date/src/test/java/team/codium/legacytraining/printdate/PrintDateTest.java
@@ -5,14 +5,24 @@ import team.codium.legacytraining.printdate.Calendar;
 import team.codium.legacytraining.printdate.PrintDate;
 import team.codium.legacytraining.printdate.Printer;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static java.util.Calendar.FEBRUARY;
+import static org.mockito.Mockito.*;
+
 
 public class PrintDateTest {
 	@Test
 	public void printDate() throws Exception {
-		PrintDate printDate = new PrintDate(new Calendar(), new Printer());
+		var ANY_DATE = new GregorianCalendar(2014, FEBRUARY, 1).getTime();
+		var calendar = mock(Calendar.class);
+		when(calendar.today()).thenReturn(ANY_DATE);
+		var printer = mock(Printer.class);
+		PrintDate printDate = new PrintDate(calendar, printer);
 
 		printDate.printCurrentDate();
 
-		// How can we test this function?
+		verify(printer).printLine(ANY_DATE.toString());
 	}
 }
