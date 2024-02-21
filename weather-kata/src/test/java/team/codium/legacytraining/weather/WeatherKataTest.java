408ba4d68d9ae4d88b590bd621495047f47f3367
Luis Rovirosa Mairlot <luis@codium.team>
2024-02-21 11:48:40 +0100

    [weather-kata] remove unneeded comment

--- weather-kata/src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
+++ weather-kata/src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
@@ -8,8 +8,6 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WeatherKataTest {
-    // https://www.metaweather.com/api/location/753692/  // Barcelona
-
     @Test
     public void xxx() throws IOException {
         new Forecast().predict("Madrid", LocalDate.now(), false);
